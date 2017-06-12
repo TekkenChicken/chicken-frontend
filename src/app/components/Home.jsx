2cf2e76ed14381f6098462fddf5e0d8940e628e8
Scott Jones <scottdj92@gmail.com>
2017-06-11 21:24:19 -0400

    Attempting dev env building and parsing UNIX timestamp

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -10,6 +10,7 @@ export default class Home extends Component {
     return (
       <div className='container is-fluid'>
         <h1 className='title'>Characters</h1>
+        <h2 className='subtitle'>All dates follow the DD-MM-YYYY format</h2>
         <div className='columns is-multiline'>
           {this.renderCharacters()}
         </div>
@@ -25,12 +26,21 @@ export default class Home extends Component {
           <Link to={"/" + stubMetaData[character].label} key={characters.length}>
             <div className='column character'>
               <h2 className='title is-4'>{stubMetaData[character].name}</h2>
-              <h4 className='subtitle is-6'>Last Updated: {stubMetaData[character].last_updated}</h4>
+              <h4 className='subtitle is-6'>Last Updated: {this.parseTime(stubMetaData[character].last_updated)}</h4>
             </div>
           </Link>
         );
       }
     }
     return characters;
+  }
+
+  parseTime(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let month = date.getMonth();
+    let day = date.getDay();
+    let year = date.getFullYear();
+    let str = day + '/' + month + '/' + year;
+    return str;
   }
 }
