cb36a6d85167bda1efae2f3301de7353cad5f8d0
Scott Jones <scott.jones@EF.com>
2017-08-18 10:43:21 -0400

    editables: update to allow for editable text within tables

--- src/app/components/CharacterDetailRow/index.jsx
+++ src/app/components/CharacterDetailRow/index.jsx
@@ -12,22 +12,80 @@ export default class CharacterDetailRow extends Component {
 
     updateRow(e) {
         console.log('clicked', e.currentTarget);
+        this.setState({editable: true});
     }
 
     render() {
-        return (
-            <tr onClick={(e) => this.updateRow(e)}>
-                <td>{this.props.data.name}</td>
-                <td>{this.props.data.notation}</td>
-                <td>{this.props.data.hit_level}</td>
-                <td>{this.props.data.damage}</td>
-                <td>{this.props.data.speed}</td>
-                <td>{this.props.data.on_block}</td>
-                <td>{this.props.data.on_hit}</td>
-                <td>{this.props.data.on_ch}</td>
-                <td>{this.props.data.properties}</td>
-                <td>{this.props.data.notes === "null" ? "" : this.props.data.notes}</td>
-            </tr>
-        );
+        if(this.state.editable) {
+            return (
+                <tr onClick={(e) => this.updateRow(e)}>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.name}/>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.notation}/>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.hit_level}/>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.damage}/>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.speed}/>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.on_block}/>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.on_hit}/>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.on_ch}/>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.properties}/>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="control">
+                            <input className="input" value={this.props.data.notes === "null" ? "" : this.props.data.notes}/>
+                        </div>
+                    </td>
+                </tr>
+            );
+        } else {
+            return (
+                <tr onClick={(e) => this.updateRow(e)}>
+                    <td>{this.props.data.name}</td>
+                    <td>{this.props.data.notation}</td>
+                    <td>{this.props.data.hit_level}</td>
+                    <td>{this.props.data.damage}</td>
+                    <td>{this.props.data.speed}</td>
+                    <td>{this.props.data.on_block}</td>
+                    <td>{this.props.data.on_hit}</td>
+                    <td>{this.props.data.on_ch}</td>
+                    <td>{this.props.data.properties}</td>
+                    <td>{this.props.data.notes === "null" ? "" : this.props.data.notes}</td>
+                </tr>
+            );
+        }
     }
 }
